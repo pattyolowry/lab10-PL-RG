41cf3c1fa8fa2ba6666697f5bf49a589117209e0
Patrick Lowry <plowry@Patios-MacBook-Pro.local>
2025-11-06 11:24:19 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,31 @@
+# https://github.com/pattyolowry/lab10-PL-RG
+# Partner 1: Patrick Lowry
+# Partner 2: Ria Gupta
 """
 calculator.py
 - Defines functions used to create a simple calculator
 
 One function per operation, in order.
 """
-# First example
+import math
 def add(a, b): 
-    pass
+    return a + b
 
+def sub(a, b):
+    return a - b
 
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError
+    return b / a
+
+def log(a, b):
+    if a == 0 or b == 0:
+        raise ValueError
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
